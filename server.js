00339559c178d6dc4d3099a0d1f10dce0fb450e9
github username <taimurmshah@gmail.com>
2019-06-07 15:07:08 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on: ${PORT}`));
+
+app.get("/", (req, res) => {
+  res.send("api running");
+});
