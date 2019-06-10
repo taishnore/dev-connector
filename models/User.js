88e3d5a55b609fc7b90ecfaf71725ecfb5dd2445
github username <taimurmshah@gmail.com>
2019-06-10 12:19:13 -0400

    user schema

---
+++ models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+//how to create a schema in node/mongo
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = User = mongoose.model("user", UserSchema);
--- server.js
+++ server.js
@@ -1,21 +1,34 @@
+//how to bring express in.
 const express = require("express");
+
+//how to connect to DB -- look at the correct file
+//does module.exports export a variable and function?
+//then require either brings in a module or variable?
 const connectDB = require("./config/db");
 
+//this starts the server; running "npm run dev" starts the server
+// with nodemon, which allows the server to process changes without
+// having to restart the server manually.
 const app = express();
 
 //connect DB
 connectDB();
 
+//this is how i start the server; the domain for this would be
+//localhost:5000/
 app.get("/", (req, res) => {
   res.send("api running");
 });
 
-//Define Routes
+//Define Routes -- these are referring to the routes files,
+//which are analogous to controllers, i think. maybe not though.
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
 
+//for the sake of deploying this app.
 const PORT = process.env.PORT || 5000;
 
+//this function actually starts the server, and designates the port.
 app.listen(PORT, () => console.log(`Server running on: ${PORT}`));
