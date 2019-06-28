b066f089afba52aa6d4b27a77f580736f6b833d5
github username <taimurmshah@gmail.com>
2019-06-28 16:34:03 -0400

    setup redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+//Redux
+import { Provider } from "react-redux";
+import store from "./store";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <>
+          <Navbar />
+          <Route exact path={"/"} component={Landing} />
+          <section className="container">
+            <Switch>
+              <Route exact path={"/register"} component={Register} />
+              <Route exact path={"/login"} component={Login} />
+            </Switch>
+          </section>
+        </>
+      </Router>
+    </Provider>
   );
-}
+};
 
 export default App;
